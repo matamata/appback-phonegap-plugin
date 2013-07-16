04a3a3c5f85f7dd8f6f720887844ac9f7cd3318c
inator <dale@xiatron.com>
2013-07-16 14:30:07 -0700

    First commit.

---
+++ README.md
@@ -0,0 +1,31 @@
+# Appback Phonegap (Cordova) Plugin
+
+This Appback plugin for Phonegap (a.k.a. Cordova) allows for easy use the Appback.com API in mobile applications.
+
+## Supported Platforms
+
+To use this plugin you must be running at least Phonegap/Cordova 2.0.  Newer versions have not been tested but may also work.  This plugin should be compatible with all mobile OS platforms supported by Phonegap.
+
+## Installation and Use
+
+To install the plugin simply add appback.js to somewhere in /www and load it from the index.html file.
+
+    <script type="text/javascript" src="appback.js"></script>
+
+Initialize the plugin and try to login a social user.
+
+    window.plugins.appback.init({
+        'appid':id,
+        'secret':secret, //get this in a secure way!
+        'login':true, //invoke the login method too
+        'debug':true, //log to troubleshoot
+        'callback':function(data) {
+            /* do something with the returned data */
+        }
+    });
+
+(More documentation coming soon. For now see the plugin for other methods)
+
+## About Appback
+
+Appback.com provides a mobile backend as a service with an easy to use API that was designed for indie developers.  We allow for you to focus on your app, not servers, by providing social integration & login, game mechanics, and NoSQL custom data.  On the horizon we have more exciting features planned, including geo-location, messaging, and analytics.  Our aim is to be the backend of choice for developers by being easy, affordable, and robust!  Head to appback.com for a free starter account!
---
+++ www/appback.js
@@ -0,0 +1,150 @@
+/*
+*   Appback Phonegap (Cordova) Plugin
+*   Copyright 2013 Xiatron LLC
+*
+*   Use a solid backend through an easy API designed for indie developers.
+*   Just go to http://appback.com to register for a free account.
+.*/
+
+(function() {
+    var appbackAppId;
+    var appbackSecret;
+    var debug;
+ 
+    /* Main appback object */
+    function appback() {};
+ 
+    /* Init method */
+    appback.prototype.init = function(options) {
+        //set degugging
+        debug = (options.debug) ? options.debug : false;
+ 
+        //small timeout to avoid conflicts
+        setTimeout(function() {
+            //make sure we have the dependancies loaded
+            if (typeof jQuery=='undefined') {
+                if (debug) console.log('Appback: Loading jQuery');
+                loadScript('http://api.appback.com/scripts/phonegap/jquery.min.js',function(){window.plugins.appback.init(options)});
+            } else if (typeof CryptoJS=='undefined') {
+                if (debug) console.log('Appback: Loading CryptoJS');
+                loadScript('http://api.appback.com/scripts/phonegap/hmac-sha256.js',function(){window.plugins.appback.init(options)});
+            } else {
+                appbackAppId = options.appid;
+                appbackSecret = options.secret;
+                
+                if (options.login) {
+                    window.plugins.appback.login({
+                        'userId':'self',
+                        'callback':options.callback
+                    });
+                } else {
+                    if (options.callback) options.callback();
+                }
+            }
+        },1000);
+    };
+ 
+    /* Social user login method*/
+    appback.prototype.login = function(options) {
+        if (debug) console.log('Appback: login invoked');
+ 
+        var url = 'https://api.appback.com/'+appbackAppId+'/social/users/self/login';
+        var sig = getAppbackSig(url);
+ 
+        //open the childbrowser
+        openRemoteChildBrowser(
+            url+'?silentflag=closeme&timestamp='+sig.timestamp+'&signature='+sig.signature,
+            function(){
+                window.plugins.appback.getUserData({
+                    'userId':options.userId,
+                    'callback':function(data) {
+                        if (options.callback) options.callback(data);
+                    }
+                });
+            }
+        );
+    }
+ 
+    /* Social user data method */
+    appback.prototype.getUserData = function(options) {
+        if (debug) console.log('Appback: getUserData invoked');
+ 
+        var url = 'https://api.appback.com/'+appbackAppId+'/social/users/'+options.userId;
+        var sig = getAppbackSig(url);
+ 
+        $.get(
+            url+'?userdata=true&timestamp='+sig.timestamp+'&signature='+sig.signature,
+            function(data) {
+                if (options.callback) options.callback(data);
+            }
+        );
+    }
+ 
+    /* Social users stats method */
+    appback.prototype.getUsersStats = function(callback) {
+        if (debug) console.log('Appback: getUsersStats invoked');
+ 
+        var url = 'https://api.appback.com/'+appbackAppId+'/social/users/';
+        var sig = getAppbackSig(url);
+ 
+        $.get(
+            url+'?timestamp='+sig.timestamp+'&signature='+sig.signature,
+            function(data) {
+                callback(data);
+            }
+        );
+    }
+ 
+    /* Internal functions */
+    function loadScript(src, callback) {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = src;
+        script.onload = (callback) ? callback : function(){};
+        document.getElementsByTagName('head')[0].appendChild(script);
+    }
+ 
+    function getAppbackSig(url) {
+        if (debug) console.log('Appback: getAppbackSig invoked');
+         
+        //generate an ios 8601 timestamp
+        var date = new Date();
+        var timestamp = date.toISOString();
+        
+        //Concatenate appid, url and timestamp
+        var message = appbackAppId+url+timestamp;
+
+        //hash and hex
+        var hash = CryptoJS.HmacSHA256(message, appbackSecret);
+        var hashHexed = CryptoJS.enc.Hex.stringify(hash);
+        if (debug) console.log('Appback: Sig hashHexed = '+hashHexed);
+        
+        return {'signature':hashHexed,'timestamp':timestamp};
+    }
+ 
+    var cbInvoked = false;
+    function openRemoteChildBrowser(url, callback) {
+        console.log('openRemoteChildBrowser invoked for '+url);
+        
+        //to ensure that CB is not invoked multiple times
+        if (cbInvoked) return false;
+        cbInvoked = true;
+        
+        window.plugins.childBrowser.onLocationChange = function(loc) {
+            if (loc.indexOf('closeme=true') > -1) {
+                window.plugins.childBrowser.close();
+            }
+        };
+        
+        window.plugins.childBrowser.onClose = function() {
+            cbInvoked = false;
+            if (callback) callback();
+        };
+                
+        window.plugins.childBrowser.showWebPage(url);
+    }
+ 
+    /* Auto-install the plugin */
+    if(!window.plugins) window.plugins = {};
+    window.plugins.appback = new appback();
+})();
